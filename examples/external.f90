0fd19c3d7397ce7eba263425dc0cfa06e95267f2
Jan-Willem Blokland <J.W.S.Blokland@XS4All.nl>
2020-01-03 21:31:04 +0100

    Reorganized the source files. Now, there is an examples directories containing profiler examples. Split the CMakeLists.txt file in three parts, a general one, one for the actual profiler library, and one for the examples. The API documentation is generated from the CMakeLists.txt file.

--- examples/external.f90
+++ examples/external.f90
@@ -1,37 +1,38 @@
-program program
+program external
   use, intrinsic :: iso_fortran_env
   use            :: profiler
   implicit none
 
   integer(int32)     :: i
   character(len=255) :: name
+  type(prof_t)       :: prof
 
   call get_command_argument(0, name)
-  call prof_init(trim(name))
+  call prof_init(prof, trim(name))
   
-  call prof_tic("level 1")
+  call prof_tic(prof, "level 1")
      call execute_command_line("sleep 0.1", wait=.true.)
 
      do i=1,2
-        call prof_tic("level 1.1", 1000_int64 * kind(i), 'B')
+        call prof_tic(prof, "level 1.1", 1000_int64 * kind(i), 'B')
           call execute_command_line("sleep 0.11", wait=.true.)
-          call prof_tic("level 1.1.1")
+          call prof_tic(prof, "level 1.1.1")
             call execute_command_line("sleep 0.111", wait=.true.)
-          call prof_toc()
-          call prof_tic("level 1.1.2")
+          call prof_toc(prof)
+          call prof_tic(prof, "level 1.1.2")
             call execute_command_line("sleep 0.112", wait=.true.)
-          call prof_toc()
-        call prof_toc()
+          call prof_toc(prof)
+        call prof_toc(prof)
      end do
         
-     call prof_tic("level 1.2")
+     call prof_tic(prof, "level 1.2")
        call execute_command_line("sleep 0.12", wait=.true.)
  
-       call prof_tic("level 1.2.1")
+       call prof_tic(prof, "level 1.2.1")
          call execute_command_line("sleep 0.121", wait=.true.)
-       call prof_toc()
-     call prof_toc()
-  call prof_toc()
+       call prof_toc(prof)
+     call prof_toc(prof)
+  call prof_toc(prof)
 
-  call prof_report()
-end program program
+  call prof_report(prof)
+end program external
---
+++ examples/internal.f90
@@ -0,0 +1,37 @@
+program internal
+  use, intrinsic :: iso_fortran_env
+  use            :: profiler
+  implicit none
+
+  integer(int32)     :: i
+  character(len=255) :: name
+
+  call get_command_argument(0, name)
+  call prof_init(trim(name))
+  
+  call prof_tic("level 1")
+     call execute_command_line("sleep 0.1", wait=.true.)
+
+     do i=1,2
+        call prof_tic("level 1.1", 1000_int64 * kind(i), 'B')
+          call execute_command_line("sleep 0.11", wait=.true.)
+          call prof_tic("level 1.1.1")
+            call execute_command_line("sleep 0.111", wait=.true.)
+          call prof_toc()
+          call prof_tic("level 1.1.2")
+            call execute_command_line("sleep 0.112", wait=.true.)
+          call prof_toc()
+        call prof_toc()
+     end do
+        
+     call prof_tic("level 1.2")
+       call execute_command_line("sleep 0.12", wait=.true.)
+ 
+       call prof_tic("level 1.2.1")
+         call execute_command_line("sleep 0.121", wait=.true.)
+       call prof_toc()
+     call prof_toc()
+  call prof_toc()
+
+  call prof_report()
+end program internal
