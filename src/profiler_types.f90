bcbbc59942b4dbc9272022f484ab71a30fceaf8e
Jan-Willem Blokland <J.W.S.Blokland@KPNMail.nl>
2024-11-28 09:32:44 +0100

    Children: (fix) Wrong timing information
    - Changed the implementation of keeping track of child watches.
      Now, we make use of linked list instead of an array. This has
      solved the problem of wrong timing information. Furthermore,
      it reduces move around when a new child watches needs to be
      added.

--- src/profiler_types.f90
+++ src/profiler_types.f90
@@ -14,15 +14,16 @@ module profiler_types
   !> \ingroup profileri
   !> \brief Derived type representing a watch.
   type, public :: watch_t
-     type(watch_t),          pointer               :: parent     => null()  !< Parent watch.
-     type(watch_t),          dimension(:), pointer :: children   => null()  !< Child watches.
-     character(len=STR_LEN)                        :: name                  !< Name of the watch.
-     integer(int32)                                :: generation =  0       !< Family generation.
-     integer(int64)                                :: nused      =  0       !< Number of times this watch has been used.
-     integer(int64)                                :: etime      =  0       !< Sum of the elasped counting time.
-     integer(int64)                                :: start      =  0       !< Starting counting time.
-     character(len=STR_LEN)                        :: unit       = ''       !< Base name of the unit.
-     integer(int64)                                :: nunits     =  0       !< Sum of the number of units that have been used.
+     type(watch_t),          pointer :: parent     => null()  !< Parent watch.
+     type(watch_t),          pointer :: sibling    => null()  !< Sibling watches
+     type(watch_t),          pointer :: children   => null()  !< Child watches.
+     character(len=STR_LEN)          :: name                  !< Name of the watch.
+     integer(int32)                  :: generation =  0       !< Family generation.
+     integer(int64)                  :: nused      =  0       !< Number of times this watch has been used.
+     integer(int64)                  :: etime      =  0       !< Sum of the elasped counting time.
+     integer(int64)                  :: start      =  0       !< Starting counting time.
+     character(len=STR_LEN)          :: unit       = ''       !< Base name of the unit.
+     integer(int64)                  :: nunits     =  0       !< Sum of the number of units that have been used.
   end type watch_t
   !> \endcond
 
